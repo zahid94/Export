e5b8fb37c132567baa25b36f3b00287270a3ceb2
zahidul islam <zahidulislam5594@gmail.com>
2020-04-16 23:45:42 +0600

    update balancesheet and incomestatement

---
+++ Export/Models/Dbcon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Export.Models
+{
+    public class Dbcon
+    {
+        public class Database
+        {
+            static string server = "108.60.209.65";
+            static string user = "aaqa_shared_dba";
+            static string pass = "Sh0hel123";
+            static string database = "RCLWEB_DB";
+
+            //static string server = "159.21.2.16";
+            //static string user = "sa";
+            //static string pass = "R0yal123";
+            //static string database = "RCLWEB_DB_NEW";
+
+            //static string server = "LAPTOP-G3Q6J4HR";
+            //static string user = "sa";
+            //static string pass = "A@qaTech123";
+            //static string database = "RCLWEB_DB";
+
+            public static string DbCon = "User ID=" + user + "; Password=" + pass + "; Initial Catalog=" + database + "; Data Source=" + server + "";
+
+            public static string DbPrefix = "wc_";
+        }
+    }
+}
